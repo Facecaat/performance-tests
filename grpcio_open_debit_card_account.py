b41b7685e30ac71c734a2b1ab64ee2a8392fd74f
Facecaat <dimjke7@gmail.com>
2026-01-10 04:46:20 +0300

    9.1 Введение в grpcio. Практика работы с grpcio

---
+++ grpcio_open_debit_card_account.py
@@ -0,0 +1,36 @@
+import grpc
+
+from contracts.services.gateway.users.rpc_get_user_pb2 import GetUserRequest, GetUserResponse
+from contracts.services.gateway.users.rpc_create_user_pb2 import CreateUserRequest, CreateUserResponse
+from contracts.services.gateway.users.users_gateway_service_pb2_grpc import UsersGatewayServiceStub
+from contracts.services.gateway.accounts.rpc_get_accounts_pb2 import GetAccountsRequest, GetAccountsResponse
+from contracts.services.gateway.accounts.rpc_open_debit_card_account_pb2 import OpenDebitCardAccountResponse, \
+    OpenDebitCardAccountRequest
+from contracts.services.gateway.accounts.accounts_gateway_service_pb2_grpc import AccountsGatewayServiceStub
+from tools.fakers import fake
+
+channel = grpc.insecure_channel("localhost:9003")
+
+users_gateway_service = UsersGatewayServiceStub(channel)
+accounts_gateway_service = AccountsGatewayServiceStub(channel)
+
+create_user_request = CreateUserRequest(
+    email=fake.email(),
+    last_name=fake.last_name(),
+    first_name=fake.first_name(),
+    middle_name=fake.middle_name(),
+    phone_number=fake.phone_number()
+)
+create_user_response: CreateUserResponse = users_gateway_service.CreateUser(create_user_request)
+print('Create user response:', create_user_response)
+
+# Создаем тело запроса для OpenDebitCard
+open_debit_card_request = OpenDebitCardAccountRequest(
+    user_id=create_user_response.user.id
+)
+# Отправляем запрос на открытие карты
+open_debit_card_response: OpenDebitCardAccountResponse = accounts_gateway_service.OpenDebitCardAccount(
+    open_debit_card_request)
+# Выводим результат
+print('Open debit card account response:', open_debit_card_response)
+
