1b5ac677017c9b8e64ce2104916b4b3a78b96ce6
Facecaat <dimjke7@gmail.com>
2026-01-11 23:06:51 +0300

    10.3 Использование HTTP API клиентов в нагрузочных сценариях. Практика: Применение HTTP API клиентов в нагрузочном сценарии

--- locust_open_debit_card_account.py
+++ locust_open_debit_card_account.py
@@ -1,28 +1,27 @@
-from locust import HttpUser, between, task
+from locust import User, between, task
 
-from tools.fakers import fake
+from clients.http.gateway.users.client import UsersGatewayHTTPClient, build_users_gateway_locust_http_client
+from clients.http.gateway.users.schema import CreateUserResponseSchema
+from clients.http.gateway.accounts.client import AccountsGatewayHTTPClient, build_accounts_gateway_locust_http_client
+from clients.http.gateway.accounts.schema import OpenDebitCardAccountResponseSchema
 
 
-class OpenDebitCardAccountScenarioUser(HttpUser):
+class OpenDebitCardAccountScenarioUser(User):
+    host = "localhost"
     wait_time = between(1, 3)
-
-    user_data: dict
+    user_gateway_client: UsersGatewayHTTPClient
+    accounts_gateway_client: AccountsGatewayHTTPClient
+    create_user_response: CreateUserResponseSchema
+    open_debit_card_response: OpenDebitCardAccountResponseSchema
 
     def on_start(self) -> None:
         """
-                Метод on_start вызывается один раз при запуске каждой сессии виртуального пользователя.
-                Здесь мы создаем нового пользователя, отправляя POST-запрос к /api/v1/users.
-                """
-        request = {
-            "email": fake.email(),
-            "lastName": fake.last_name(),
-            "firstName": fake.first_name(),
-            "middleName": fake.middle_name(),
-            "phoneNumber": fake.phone_number()
-        }
-        response = self.client.post("/api/v1/users", json=request)
-
-        self.user_data = response.json()
+        Метод on_start вызывается один раз при запуске каждой сессии виртуального пользователя.
+        Здесь мы создаем нового пользователя, отправляя POST-запрос к /api/v1/users.
+        """
+        self.users_gateway_client = build_users_gateway_locust_http_client(self.environment)
+        self.create_user_response = self.users_gateway_client.create_user()
+        self.accounts_gateway_client = build_accounts_gateway_locust_http_client(self.environment)
 
     @task
     def open_debit_card_account(self):
@@ -30,8 +29,4 @@ class OpenDebitCardAccountScenarioUser(HttpUser):
         Основная нагрузочная задача: открытие дебетовой карты.
         Здесь мы выполняем POST-запрос к /api/v1/accounts/open-debit-card-account
         """
-        self.client.post(
-            f"/api/v1/accounts/open-debit-card-account",
-            name="/api/v1/accounts/open-debit-card-account",
-            json={"userId": self.user_data['user']['id']}
-        )
+        self.accounts_gateway_client.open_debit_card_account(self.create_user_response.user.id)
